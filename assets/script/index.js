24f455184adb5b5686c09f55a391add8c64819b7
Adogamhe Aleogho Martins(Jnr.) Andre <martinsadogamhe9@gmail.com>
2024-03-04 19:54:07 -0600

    Added styles
    Added some styles and made the functions work

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,15 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const display = document.querySelector('[name="display"]');
     const buttons = document.querySelectorAll('input[type="button"]');
 
+// Functions that makes the operators work
     buttons.forEach(button => {
         button.addEventListener('click', function() {
             const value = this.value;
             if (value === 'AC') {
                 display.value = '';
-            } else if (value === 'Del') {
-                display.value = display.value.slice(0, -1);
+            } else if (value === '±') {
+                display.value = (display.value * -1);
             } else if (value === '=') {
                 display.value = eval(display.value);
+            } else if (value === '%') {
+                display.value = (display.value / 100);
             } else {
                 display.value += value;
             }
